85e62953b740773d1e0795a0e125c13d6fff0e00
Alejandra Rivera <aleriveram19@gmail.com>
2024-08-13 18:46:54 -0700

    Finalización del Proyecto

--- src/task.js
+++ src/task.js
@@ -1,5 +1,8 @@
 // Para Obtenr las lista de tareas del almacenamiento del navegador 
-let tasks = JSON.parse(localStorge.getItem('tasks')) || [];
+let tasks =JSON.parse(localStorage.getItem('tasks')) || [];
+
+//Función para poder llevar las tareas de las lista a otros archivos de JS
+export const getTasks =  () => tasks;
 
 // Funcion para agregar una tareas a las lista de tareas
 export const addTask = (task) => {
@@ -7,6 +10,26 @@ export const addTask = (task) => {
         id: Date.now(),
         text: task,
         completed: false,
-    }
+    };
+    tasks.push(newTask);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+//Función para eliminar una tarea de la lista 
+export const deleteTask = (id) => {
+    tasks = tasks.filter((task) => task.id !== parseInt(id));
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+// FuNCION PARA ACATUALIZAR UNA ATREA DE LA LISTA
+export const toggleTask = (id) =>{
+    tasks = tasks.map((task) => {
+        if(task.id === parseInt(id)) {
+            task.completed = !task.completed;
+        }
+        return task;
+        
+    });
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,23 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/index.js' //Punto de entrada de tu aplicación, este es un archivo de entrada para que webpack empiece a construir
-    output: {
-        filename:'bundle.js,'
-        path: path.resolve(__dirname, dist 'dist') //Carpeta de salida 
+module.exports ={
+    mode: 'development',
+    entry: './src/index.js', //Punto de entrada de tu aplicación, este es un archivo de entrada para que webpack empiece a construir
+    output:{
+        filename:'bundle.js', //Nombre del archivo de salida
+        path: path.resolve(__dirname, 'dist'), //Carpeta de salida 
     },
     module: {
         rules: [
             {
-                test: /\.css$/ , 
+                test: /\.css$/, //Regex para identificar archivos css
                 use: ['style-loader', 'cssloader'], //Loaders para procesar archivos
             },
             {
                 test: /\.js$/, //Regex para identificar archivos JS
                 exclude: /node_modules/, //Excluir la carpeta node_modules
                 use: {
-                    loader: 'babel-loader', //Loader para transpilar JS moderno a JS compatible con navegadores
+                    loader:'babel-loader', //Loader para transpilar JS moderno a JS compatible con navegadores
                     options: {
                         presets:  ['@babel/preset-env'], //Preset de Babel para convertir JS moderno a versines mas antiguas
                     }
@@ -26,7 +27,9 @@ module.exports = {
     },
     devtool: 'source-map', //Generar source maps para facilitar la depuración
     devServer: {
-        contentBase: path.resolve(__dirname, 'dist'), //Carpeta desde donde obtenemos los archivos que mostaramos al usuario
+        static: {
+            directory:path.resolve(__dirname, 'dist'),
+        },
         compres: true, //Habilitando compresión gzip, reduce los tamaños de los archivos mejorando la carga
         port: 9000, //Puerto del servidor de desarrollo
     }
